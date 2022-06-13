32ef173d27099718316479ca66994a8fb57cfb0b
Rafael Silva e Souza <rafael.silva@gmail.com>
2022-06-13 15:55:38 -0300

    Whoops.

--- packages/swayze/lib/src/core/controller/selection/user_selections/user_selection_state.dart
+++ packages/swayze/lib/src/core/controller/selection/user_selections/user_selection_state.dart
@@ -1,6 +1,5 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:swayze_math/swayze_math.dart';
 
